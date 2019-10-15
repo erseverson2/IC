ee36bf9af3bc781a306684c64a7336a0f1463701
junlin0320 <36519710+junlin0320@users.noreply.github.com>
2019-10-15 13:52:32 -0500

    Add files via upload

--- cpu.v
+++ cpu.v
@@ -41,7 +41,7 @@ module cpu(clk, rst_n, hlt, pc);
 
 	/////////////// Control Signals //////////////
 	wire [3:0]opcode;
-	wire [2:0]FLAGS;
+	wire [2:0]FLAGS, ALU_FLAG_out;
 	assign opcode = imem_data_out[15 : 12];
 	
 	// ALUSRC controls if RegisterSrcData2 or Signextedimm goes in to ALU src 2, 1 for offset, 0 for Reg_out2
@@ -63,16 +63,48 @@ module cpu(clk, rst_n, hlt, pc);
 	// MemWrite determines if [15:0] data from Register output 2 gets writen into address from ALU output
 	assign MemWrite = 1;
 
-	// PC source determines where the next PC will be coming from, PC_control, or register 
+	// BranchType 0 if its Branch immediate ins, 1 if Branch Register
+	assign BranchType = 1;
+
+	// BranchIns, 1 if opcode is a branch instruction, 0 if not
+	assign BranchIns = 1;
+
+	// Halt
+	assign Halt = &opcode;
+
+	// LBIns, 1 if opcode is LoadBype instruction, 0 otherwise
+	assign LBIns = 0;
+
+	// PCtoReg, 1 if want to write PC to dstReg
+	assign PCtoReg = 0;
 
 	///////////// Control Signals END//////////////
 
+	///////////// FLAG Register /////////////////////////
+	FlagRegister iFG(.flag_reg_input(ALU_FLAG_out), .flag_reg_output(FLAGS), .clk(clk), .wen(~Halt), .rst(rst_reg));
+
+	///////////// FLAG REGISTER END /////////////////////
+
+	// Register and ALU wires
+	wire [15:0] reg_data1, reg_data2, reg_wrt_data, SrcReg1_in, SrcReg2_in;
+	wire [15:0] ALU_In1, ALU_In2, RegFile_SrcData2, ALU_Out, ALU_mux_out, loaded_byte;
 
 	////////////// PC and PC control /////////////////////
 	wire [15:0] PC_out, PC_in;
 	PC iPC(.clk(clk), .rst(rst_reg), .write_en(1'b1), .PC_in(PC_in), .PC_out(PC_out));
 	//PC control needs to be changed to take care of branch register ins
-	PC_control iPC_control(.C(imem_data_out[11:9]), .I(imem_data_out[8:0]), .F(FLAGS), .PC_in(PC_out), .PC_out(PC_in));
+	PC_control iPC_control(
+	.C(imem_data_out[11:9]), 
+	.I(imem_data_out[8:0]),
+	.F(FLAGS),
+	.PC_control_in(PC_out),
+	.reg2_data(reg_data1),
+	.branch_type(BranchType),
+	.halt(Halt),
+	.branch_ins(BranchIns),
+	.PC_control_out(PC_in)
+	);
+
 
 	////////////// PC control END /////////////////
 
@@ -85,24 +117,32 @@ module cpu(clk, rst_n, hlt, pc);
 
 	memorylc DMEM(.data_out(dmem_data_out), .data_in(dmem_data_in), .addr(dmem_addr),
 			.enable(dmem_enable), .wr(dmem_wr), .clk(clk), .rst(~rst_n));
+
+	
 	/////////////// D-MEM END////////////////////////
 
 	/////////////// Registers //////////////////////
 	// @ imem_data_out[7:4] is Rs
 	// @ imem_data_out[3:0] is Rt in [OPCODE][Rd][Rs][Rt/imm]
 	// @ imem_data_out[11:8]
-	wire [15:0] reg_data1, reg_data2, reg_wrt_data, SrcReg1_in, SrcReg2_in;
+	
 	// Reg2Src determines which bits from the instruction is going to used as the address in register src 2. 1 for []
 	assign SrcReg1_in = imem_data_out[7:4];
 	assign SrcReg2_in = Reg2Src? imem_data_out[11:8] :imem_data_out[3:0];
 	RegisterFile IREGFILE(.clk(clk), .rst(rst_reg), .SrcReg1(SrcReg1_in), .SrcReg2(SrcReg2_in), .DstReg(imem_data_out[11:8]), 
 			.WriteReg(RegWrite), .DstData(reg_wrt_data), .SrcData1(reg_data1), .SrcData2(reg_data2));
+
+	assign reg_wrt_data = MemtoReg ? dmem_data_out : (PCtoReg ? PC_out: ALU_mux_out);
 	/////////////// Registers End///////////////////
 
-	/////////////// ALU ////////////////////////////
+	/////////////// ALU ///////////////////////////
 	wire[2:0] ALU_Opcode = pc[13:11];
-	wire[15:0] ALU_In1, ALU_In2, RegFile_SrcData2, ALU_Out;
+	
 
+	// handle Load Byte instructions
+	// if opcode[0] is true, then it is load higher byte, lower byte otherwise
+	assign loaded_byte = opcode[0]? ({{imem_data_out[7:0]},{reg_data2[7:0]}}): ({{reg_data2[15:8]},{imem_data_out[7:0]}});
+	assign ALU_mux_out = LBIns ? loaded_byte : ALU_Out; 
 	// ALUSRC controls if RegisterSrcData2 or Signextedimm goes in to ALU src 2, 1 for offset, 0 for Reg_out2
 	assign ALU_In2 = ALUSRC ? {{12{imem_data_out[3]}}, imem_data_out[3:0]}: RegFile_SrcData2;
 
